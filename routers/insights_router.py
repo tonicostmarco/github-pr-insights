8e57e3ba3decb7ade9543d98de29236a67f00f08
tonicostmarco <marcoricardo61b@gmail.com>
2026-05-22 21:03:41 -0300

    feat(router): add Authorization header validation to insights endpoint

--- routers/insights_router.py
+++ routers/insights_router.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter
 
 from services import analytics_service
+from fastapi import Header
 
 insights_router = APIRouter(prefix="/insights", tags=["insights"])
 
 @insights_router.get("/")
-async def insights(date_from, date_to):
+async def insights(date_from, date_to, authorization: str = Header(None)):
+    if authorization is None:
+        return {"status": "Unauthorized", "result": []}
+    else:
+        token = authorization.split(" ")[1]
 
-    response = analytics_service.init(date_from, date_to)
+        response = analytics_service.init(date_from, date_to, token)
 
-    return {"status": "ok", "result": response}
+        return {"status": "ok", "result": response}
